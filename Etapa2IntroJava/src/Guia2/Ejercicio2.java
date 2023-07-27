0047ab9b89deae489f67469dc776fa2379eac139
Brisa Argañaraz <brisaarganaraz2@gmail.com>
2023-07-27 20:10:39 -0300

    proyecto creado y archivos eliminados

---
+++ Etapa2IntroJava/src/Guia2/Ejercicio2.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Guia2;
+
+/**
+ *
+ * @author Hogar
+ */
+public class Ejercicio2 {
+    
+}

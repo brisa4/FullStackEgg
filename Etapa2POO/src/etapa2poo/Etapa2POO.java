cb7780e40cfada1692aa25b31caccf54ac6fc1a8
Brisa Argañaraz <brisaarganaraz2@gmail.com>
2023-07-31 20:44:20 -0300

    ...

--- Etapa2POO/src/etapa2poo/Etapa2POO.java
+++ Etapa2POO/src/etapa2poo/Etapa2POO.java
@@ -3,12 +3,19 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Etapa2POO.ENTIDAD;
+package etapa2poo;
 
 /**
  *
  * @author Hogar
  */
-public class HOLA {
+public class Etapa2POO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
     
 }

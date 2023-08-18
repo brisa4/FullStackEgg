524ae776d2fcc0bd7498af208f43c50ebf571878
Brisa Argañaraz <brisaarganaraz2@gmail.com>
2023-08-18 19:35:00 -0300

    ...

---
+++ Etapa2POO/src/Ejercicio1/main/Main.java
@@ -0,0 +1,14 @@
+package Ejercicio1.main;
+
+import Ejercicio1.entidad.Libro;
+
+public class Main {
+   public static void main(String[] args) {
+        Libro primerLibro = new Libro(12321, "Esta noche digo adios", "Jeremias", 234);
+        Libro segundoLibro = new Libro();
+        
+        segundoLibro.cargarLibro();
+        segundoLibro.mostrarLibro();
+        primerLibro.mostrarLibro();
+    }  
+}
